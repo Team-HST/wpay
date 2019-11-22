f115198c7ba3727b635faaf9a99a9d8e0d2066a8
dudgns0612 <dudgns0612@naver.com>
2019-11-22 17:53:41 +0900

    [Frontend > service] 초기 세팅 진행중

--- frontend/service/src/main.js
+++ frontend/service/src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-import store from './store'
+import store from './store/index'
 import axios from 'axios'
+import lodash from 'lodash'
+
+// Components
+import './components'
 
 // axios 설정
 Vue.prototype.$http = axios;
+Vue.prototype._ = lodash;
 
 new Vue({
   store,
