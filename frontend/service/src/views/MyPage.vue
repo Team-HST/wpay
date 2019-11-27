9e142ba417957bda8c7120fb285c4dfecfbe469f
dudgns0612 <dudgns0612@naver.com>
2019-11-27 21:57:00 +0900

    [Frontend > service] mypage 화면완료 API 적용필요

---
+++ frontend/service/src/views/MyPage.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <layout-header></layout-header>
+    <v-content>
+      <v-container
+        class="mt-10 fill-height grid-list-xl"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="12"
+            lg="6"
+            sm="8"
+            md="8"
+          >
+            <material-card
+              color="pink lighten-2"
+              title="마이페이지"
+              text="지출과 정산내역을 확인합니다."
+            >
+              <mypage-tab></mypage-tab>
+            </material-card>
+          </v-col>
+        </v-row>
+      </v-container> 
+    </v-content>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tab: null
+    }
+  }
+}
+</script>
