85cfbb75f3a8995ffe8b2b9646d7955d7717ab13
shl9012 <zoflrjs2145@naver.com>
2019-11-29 00:58:14 +0900

    [Frontend > admin] api 연결작업 중

--- frontend/admin/src/components/wedding/Calculate.vue
+++ frontend/admin/src/components/wedding/Calculate.vue
@@ -14,13 +14,25 @@
                 <v-flex xs6 style="text-align: right;">
                   <span class="title" color="font-weight-bold"> {{calculate.totalCelebrationPrice}} 원 </span>
                 </v-flex>
+                <v-flex xs6>
+                  <span class="title">신랑 측</span>
+                </v-flex>
+                <v-flex xs6 style="text-align: right;">
+                  <span class="title" color="font-weight-bold"> {{calculate.maleHostTotalCelebrationAmount}} 원 </span>
+                </v-flex>
+                <v-flex xs6>
+                  <span class="title">신부 측</span>
+                </v-flex>
+                <v-flex xs6 style="text-align: right;">
+                  <span class="title" color="font-weight-bold"> {{calculate.femaleHostTotalCelebrationAmount}} 원 </span>
+                </v-flex>
               </v-row>
               <v-row class="ma-4 pa-5" style="border:1px solid #EC407A; border-radius: 2em;">
                 <v-flex xs8>
                   <span class="title">발급된 식권 개수</span>
                 </v-flex>
                 <v-flex xs4 style="text-align: right;">
-                  <span class="title" color="font-weight-bold"> {{calculate.totalMealCnt}} 개 </span>
+                  <span class="title" color="font-weight-bold"> {{calculate.totalMealTicketCount}} 개 </span>
                 </v-flex>
                 <v-flex class="mt-2" xs6>
                   <span class="title">총 식대비</span>
@@ -34,7 +46,7 @@
                   <span class="title">차 액</span>
                 </v-flex>
                 <v-flex xs6 style="text-align: right;">
-                  <span class="title" color="font-weight-bold"> {{calculate.diffPrice}} 원 </span>
+                  <span class="title" color="font-weight-bold"> {{calculate.remainingAmount}} 원 </span>
                 </v-flex>
               </v-row>
               <v-row class="ma-12">
@@ -63,40 +75,57 @@
 </template>
 
 <script>
-// import { mapActions } from 'vuex';
+import { mapGetters } from 'vuex';
 import { api } from '@/utils/api';
+
 export default {
   data: () => ({
     calculate: {
-      seq: 0,                   // 일련번호
       weddingSeq: 0,            // 결혼일련번호
       totalCelebrationPrice: 0, // 총 축의금
-      totalMealCnt: 0,          // 총 식권갯수
+      femaleHostTotalCelebrationAmount: 0,  // 신부 측 축의금
+      maleHostTotalCelebrationAmount: 0,    // 신랑 측 축의금
+      totalMealTicketCount: 0,          // 총 식권갯수
       totalMealPrice: 0,        // 총 식대비
-      diffPrice: 0              // 차액
+      remainingAmount: 0              // 차액
     }
   }),
+  computed: {
+    ...mapGetters(['getUserData'])
+  },
   created() {
-    this.calculate.weddingSeq = this.$route.params.weddingSeq;
+    alert('thi'+this.$route.params.seq);
+    this.calculate.weddingSeq = this.$route.params;
     this.service = {
-      searchWedding: (weddingDt) => {
-        /* 수정필요 */
-        api.auth.post('/api/wedding/list', weddingDt)
+      /* 정산 API */
+      calculate: () => {
+        api.setUserToken(this.getUserData.token);
+        api.auth.get('/api/weddings/'+this.calculate.weddingSeq+'/settle')
         .then(response => {
-            return response;
+            if (response !== "undefined") {
+              this.calculate.totalCelebrationPrice = this.numberFormat(response.data.totalCelebrationAmount);
+              this.calculate.femaleHostTotalCelebrationAmount = this.numberFormat(response.data.femaleHostTotalCelebrationAmount);
+              this.calculate.maleHostTotalCelebrationAmount = this.numberFormat(response.data.maleHostTotalCelebrationAmount);
+              this.calculate.totalMealPrice = this.numberFormat(response.data.totalMealPrice);
+              this.calculate.totalMealTicketCount = this.numberFormat(response.data.totalMealTicketCount);
+              this.calculate.remainingAmount = this.numberFormat(response.data.remainingAmount);
+            }
         });
       }
     }
   },
   mounted() {
-    //this.initialize();
+    this.initialize();
   },
   methods: {
-    createCalculate: function() {
-
+    initialize: function() {
+      this.service.calculate();
     },
     moveWeddingList: function() {
       this.$router.push({name:"WeddingList"});
+    },
+    numberFormat: function(inputNumber) {
+      return inputNumber.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     }
   }
 }
