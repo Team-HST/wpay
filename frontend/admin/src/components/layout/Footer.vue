f118be2bdd37b7c8e7eeb4efc186a88385988782
shl9012 <zoflrjs2145@naver.com>
2019-11-30 00:30:49 +0900

    [Frontend > admin] 버그수정

--- frontend/admin/src/components/layout/Footer.vue
+++ frontend/admin/src/components/layout/Footer.vue
@@ -1,10 +1,7 @@
 <template>
     <v-bottom-navigation v-model="bottomNav" app background-color="pink lighten-2" grow dark shift fixed>
-      <v-btn :to="{name: 'todoPage'}" height="100%">
-        <span>ToDo</span><v-icon>mdi-newspaper-variant-multiple</v-icon>
-      </v-btn>
-      <v-btn to="/settings" height="100%">
-        <span>혼주관리</span><v-icon>mdi-settings</v-icon>
+      <v-btn height="100%">
+        <v-icon>mdi-newspaper-variant-multiple</v-icon>
       </v-btn>
     </v-bottom-navigation>
 </template>
--- frontend/admin/src/views/Login.vue
+++ frontend/admin/src/views/Login.vue
@@ -29,6 +29,9 @@
 <script>
 export default {
   data() {
+    return {
+      
+    }
   }
 };
 </script>
