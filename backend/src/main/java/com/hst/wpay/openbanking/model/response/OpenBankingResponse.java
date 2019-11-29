5bfeabd65d9ff0f8b84d31e2c64994dff8174efd
hanull <dkfghg3@naver.com>
2019-11-29 22:51:12 +0900

    [Backend] 이체API 날짜 진짜 최종 수정.

--- backend/src/main/java/com/hst/wpay/openbanking/model/response/OpenBankingResponse.java
+++ backend/src/main/java/com/hst/wpay/openbanking/model/response/OpenBankingResponse.java
@@ -1,5 +1,6 @@
 package com.hst.wpay.openbanking.model.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.hst.wpay.openbanking.model.OpenBankingConstants;
 import lombok.Data;
@@ -15,7 +16,11 @@ public class OpenBankingResponse {
 	private String rspCode;
 	@JsonProperty("rsp_message")
 	private String rspMessage;
-
+	
+	@JsonProperty("bank_tran_date")
+	@JsonFormat(pattern = "yyyy-MM-dd kk:mm:ss")
+	private String bank_tran_date;
+	
 	public boolean isSuccess() {
 		return OpenBankingConstants.RSP_CODE_SUCCESS.equals(this.rspCode);
 	}
